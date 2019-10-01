cc70761c21486e3b5ed6c443470788a5e9da77dd
nishinaga <Sion1073@gmail.com>
2019-10-01 18:14:05 +0900

    TextSendMessageのimportを追加

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
 
 app = Flask(__name__)
 
