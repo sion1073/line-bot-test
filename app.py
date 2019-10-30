197a79f58b38b2de4d9604007108627b99fb2840
nishinaga <Sion1073@gmail.com>
2019-10-30 20:58:59 +0900

    名前登録後のレスポンスを追加

--- app.py
+++ app.py
@@ -122,6 +122,7 @@ def response_message(event):
         if name:
             print(name.group(0))
             update_ids(event, name.group(0))
+            messages = TextSendMessage(text='名前登録ありがとうございます。\n' + name.group(0) + 'で登録しました！')
 
     else:
         messages = TextSendMessage(text='すみません、よくわかりません')
